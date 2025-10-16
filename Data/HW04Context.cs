17ad626415ec4ec7c6c94a310c31dde1990775d6
YinCheng <99303523+YinCheng0106@users.noreply.github.com>
2025-10-16 20:35:15 +0800

    加入專案檔案。

---
+++ Data/HW04Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HW04.Models;
+
+namespace HW04.Data
+{
+    public class HW04Context : DbContext
+    {
+        public HW04Context (DbContextOptions<HW04Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HW04.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HW04.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(100)]
+        [Display(Name="產品名稱")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name="價格 (NTD)")]
+        public decimal Price { get; set; }
+    }
+}
